e752bf9e22fb8f3479464a518d8c7c71842a0dfe
Glitch (harsh-married-toucan) <none>
2023-04-01 06:29:27 +0000

    ⛄️🐔 Checkpoint ./komutlar/aşk-ölç.js:56284628/1846

--- komutlar/aşk-ölç.js
+++ komutlar/aşk-ölç.js
@@ -1,5 +1,18 @@
 const { Permissions,Client,CommandInteraction,MessageEmbed,MessageActionRow, MessageButton, MessageSelectMenu } = require("discord.js");
-
+const faiz = [
+    "10%-Sizden arkadaş bile olmaz", 
+    "20%-En büyük aşklar kavgayla başlar.",
+    "30%-Belki ileride",
+    "40%-Deneye bilirsiniz!",
+    "70%-İdare eder",
+    "80%-Sevgili",
+    "100%",
+    "Madison'daki bir matematik öğretmeninin sahip olduğu dünyanın en zeki domuzu çarpım tablosunu 12'lere kadar ezberlemişti.",
+    " Antik Yunan'da zengin aile çocukları hayatları boyunca kılsız olmaları için doğdukları anda zeytinyağına batırılırlardı..",
+    "Dünyanın en geniş yolu olan Brezilya'daki Anıtsal Eksen'de 160 araba yan yana gidebilir.",
+    "Japon balıklarının hatırlama ömürleri yaklaşık 3 saniyedir.",
+    "Gıda renklendiricileri eklenmeseydi eğer, kolanın rengi yeşil olurdu."
+]
 module.exports = {
   name: "aşk-ölç",
   description: 'Üyeye timeout atar/kaldırır',
