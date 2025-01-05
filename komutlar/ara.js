e1f24691998201a44ebd95624c5d804ac6221237
senotron <105975100+senotron@users.noreply.github.com>
2025-01-05 15:38:28 +0400

    Update ara.js

--- komutlar/ara.js
+++ komutlar/ara.js
@@ -1,99 +1,55 @@
-const { Permissions,Client,CommandInteraction,MessageEmbed,MessageActionRow, MessageButton, MessageSelectMenu } = require("discord.js");
+const { Permissions, Client, CommandInteraction, MessageEmbed, MessageActionRow, MessageSelectMenu } = require("discord.js");
+
 module.exports = {
-  name: "ara",
-  description: "Seçtiğin numarayı ararsın",
-  type:1,
-  options: [
-    
-  ],
-  /**
-   * 
-   * @param {Client} client 
-   * @param {CommandInteraction} interaction 
-   * @returns 
-   */
+  name: "call",
+  description: "Call the number you choose",
+  type: 1,
+  options: [],
   run: async (client, interaction) => {
-    
-
-
-    const member = interaction.member;
-   const guild = interaction.guild;
-
-
-
-              
-        //Seçenek kısmı
-       const row = new MessageActionRow()
-      .addComponents(
-        new MessageSelectMenu()
-        .setCustomId("ara") 
-        .setPlaceholder('Numaralar')
+    const row = new MessageActionRow().addComponents(
+      new MessageSelectMenu()
+        .setCustomId("call")
+        .setPlaceholder("Select a number")
         .setMinValues(1)
         .setMaxValues(1)
         .addOptions([
-          
           {
-            label:"155" ,
-            description:"Polisi ararsın",
-            value:"155",
-            emoji:"👨🏻‍✈️" 
+            label: "155",
+            description: "Call the police",
+            value: "155",
+            emoji: "👨🏻‍✈️",
           },
-                    {
-            label:"112" ,
-            description:"Ambulans ara",
-            value:"112",
-            emoji:"🚑" 
+          {
+            label: "112",
+            description: "Call an ambulance",
+            value: "112",
+            emoji: "🚑",
           },
-          ])
-        )
-        await interaction.reply({content: `Aramak istediğin numarauyı seç`, components: [row]})
-        client.on("interactionCreate", async interaction => {
-          if (!interaction.isSelectMenu()) return;
-          if(interaction.customId === "ara") {
-             
-   
-     if(interaction.values[0] === "155") { 
-      const embed = new MessageEmbed()
-.setDescription(`${interaction.member} **Polis Geliyor** 👮🏽‍♂️`)
-      .setColor("BLACK")
-      .setFooter(
-        `${interaction.member.user.username} tarafından istendi.`
-      )
-      .setImage(
-        `https://www.hareketligifler.net/data/media/1449/polis-arabasi-hareketli-resim-0008.gif`
-      );
-   interaction.reply({embeds: [embed]})
-    } 
-           else if(interaction.values[0] === "112") { 
- const embed = new MessageEmbed()
-.setDescription(`${interaction.member} **Ambulans Geliyor** 🚑`)
-      .setColor("BLACK")
-      .setFooter(
-        `${interaction.member.user.username} tarafından istendi.`
-      )
-      .setImage(
-        `https://www.hareketligifler.net/data/media/937/ambulans-hareketli-resim-0016.gif`
-      );
-   interaction.reply({embeds: [embed]})     
-    } 
-          
-          
-          
-          
-          
-          }}) 
-    
-        
-        
-        
+        ])
+    );
 
-    
-       }
+    await interaction.reply({ content: `Select the number you want to call`, components: [row] });
 
-  
+    client.on("interactionCreate", async (interaction) => {
+      if (!interaction.isSelectMenu()) return;
+
+      if (interaction.customId === "call") {
+        if (interaction.values[0] === "911") {
+          const embed = new MessageEmbed()
+            .setDescription(`${interaction.member} **The police are coming!** 👮🏽‍♂️`)
+            .setColor("BLACK")
+            .setFooter(`${interaction.member.user.username} requested this.`)
+            .setImage(`https://www.hareketligifler.net/data/media/1449/polis-arabasi-hareketli-resim-0008.gif`);
+          interaction.reply({ embeds: [embed] });
+        } else if (interaction.values[0] === "112") {
+          const embed = new MessageEmbed()
+            .setDescription(`${interaction.member} **The ambulance is coming!** 🚑`)
+            .setColor("BLACK")
+            .setFooter(`${interaction.member.user.username} requested this.`)
+            .setImage(`https://www.hareketligifler.net/data/media/937/ambulans-hareketli-resim-0016.gif`);
+          interaction.reply({ embeds: [embed] });
+        }
+      }
+    });
+  },
 };
-
-
-
-
-//LetCode MrSn
