72b7e6957b7493ea728f1dc15b36c00424be23b9
senotron <105975100+senotron@users.noreply.github.com>
2025-01-05 17:42:28 +0400

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -9,11 +9,11 @@ const fs = require("fs");
 module.exports = async (client, interaction) => {
     if (interaction.isCommand()){
     try {
-      fs.readdir("./komutlar/", (err, files) => {
+      fs.readdir("./commands/", (err, files) => {
         if (err) throw err;
 
         files.forEach(async (f) => {
-          const command = require(`../komutlar/${f}`);
+          const command = require(`../commands/${f}`);
           if (
             interaction.commandName.toLowerCase() === command.name.toLowerCase()
           ) {
