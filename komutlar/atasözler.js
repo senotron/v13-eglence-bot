fb68fc8e29266cb8dc7e1d73e6042e39c1304467
senotron <105975100+senotron@users.noreply.github.com>
2025-01-05 16:54:04 +0400

    Update atasözler.js

--- komutlar/atasözler.js
+++ komutlar/atasözler.js
@@ -1,50 +1,39 @@
-const { MessageEmbed, Client, CommandInteraction,MessageButton,MessageActionRow } = require("discord.js");
+const { MessageEmbed, Client, CommandInteraction } = require("discord.js");
 
 const atasözleri = [
-    "Abanın kadri yağmurda bilinir", 
-    "Acele bir ağaçtır, meyvesi pişmanlık.",
-    "Acele ile menzil alınmaz.",
-    "İki baş bir kazanda kaynamaz.",
-    "İki cambaz bir ipte oynamaz.",
-    "Rüşvet kapıdan girince iman bacadan çıkar.",
-    "Sakla samanı gelir zamanı.",
-    "Tatlı dil yılanı deliğinden çıkarır.",
-    "Vakitsiz Öten Horozun Başını Keserler.",
-    "Zararın neresinden dönülse kardır."
-]
+  "Abanın kadri yağmurda bilinir",
+  "Acele bir ağaçtır, meyvesi pişmanlık.",
+  "Acele ile menzil alınmaz.",
+  "İki baş bir kazanda kaynamaz.",
+  "İki cambaz bir ipte oynamaz.",
+  "Rüşvet kapıdan girince iman bacadan çıkar.",
+  "Sakla samanı gelir zamanı.",
+  "Tatlı dil yılanı deliğinden çıkarır.",
+  "Vakitsiz Öten Horozun Başını Keserler.",
+  "Zararın neresinden dönülse kardır."
+];
+
 module.exports = {
   name: "atasözleri",
-  description: "atasözleri",
+  description: "Random Turkish Proverbs",
   options: [],
   /**
    * @param {Client} client
    * @param {CommandInteraction} interaction
    */
   run: async (client, interaction) => {
-    const user = interaction.member.user
-        const Embed = new MessageEmbed();
+    const user = interaction.member.user;
+    const atasöz = atasözleri[Math.floor(Math.random() * atasözleri.length)];
 
+    const embed = new MessageEmbed()
+      .setTitle(`${user.username}, Here's a Turkish Proverb for You!`)
+      .setDescription(`**"${atasöz}"**`)
+      .setColor("RANDOM")
+      .setFooter("LetCode | MrSn")
+      .setTimestamp();
 
-
-    const atas = atasözleri[Math.floor(Math.random() * atasözleri.length)]
-
-    const workEmbed = new MessageEmbed()
-        .setDescription(`**\ ${atas} \** `)
-        .setColor("RANDOM")
-
-    interaction.reply({
-      embeds: [workEmbed]
-      
+    await interaction.reply({
+      embeds: [embed],
     });
-
-    
-
-  }, 
+  },
 };
-
-
-//LetCode MrSn
-
-//LetCode MrSn
-
-//LetCode MrSn
